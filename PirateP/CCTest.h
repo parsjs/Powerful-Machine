e0d20ef93efbe11d1d151cec69c5b3ce9ac9af5b
Alireza <alireza@Alirezas-MacBook-Pro.local>
2014-09-01 00:47:42 +0600

    added ad test class

---
+++ PirateP/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  PirateP
+//
+//  Created by Alireza on 9/1/14.
+//  Copyright (c) 2014 OSX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ PirateP/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  PirateP
+//
+//  Created by Alireza on 9/1/14.
+//  Copyright (c) 2014 OSX. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
